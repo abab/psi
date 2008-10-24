b5e44132eb6dcfef83b9c3b4d5142a9662f02e85
AlekSi <alek.silverstone@gmail.com>
2008-10-24 21:56:50 +0400

    fixed compilation without XMPP::Jid's deprecated methods

--- proto/main.cpp
+++ proto/main.cpp
@@ -14,14 +14,14 @@ int main(int argc, char* argv[])
 	const int messagesCount = 5;
 	QDateTime start = QDateTime::currentDateTime().toUTC().addYears(-1);
 	for(int contactCo=1; contactCo<=contactsCount; ++contactCo) {
-		XMPP::Jid contactJid(QString("node%1@domain.com").arg(contactCo));
+		QString contact = QString("node%1@doman.com").arg(contactCo);
 		for(int collectionCo=1; collectionCo<=collectionsCount; ++collectionCo) {
 			CollectionType type = ChatCollection;
 			if(!(contactCo%3)) {
 				type = MucCollection;
-				contactJid.setDomain(QString("conf%1.domain.com").arg(contactCo));
-				contactJid.setNode("room");
+				contact = QString("room@conf%1.domain.com").arg(contactCo);
 			}
+			XMPP::Jid contactJid(contact);
 			History::CollectionInfo c = s->newCollection(type, XMPP::Jid("test@owner.com"), contactJid, start);
 			s->setCollectionSubject(c.id(), QString("Subject %1 - %2").arg(contactCo).arg(collectionCo));
 			s->newEntry(c.id(), NoteEntry, contactJid, "me", "private note", start);
